db716f44f85837bf23ca50bfbedadceade7742f3
Sea10wood <miyusea5@gmail.com>
2023-12-16 12:57:03 +0900

    page分け＋CuraterPage作成

---
+++ ChristmasCurator/frontend/src/curater.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+// import "./App.css";
+import "./ChristmasTree.css";
+import { Greet } from "../wailsjs/go/main/App";
+
+function Curater() {
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+  return (
+    <div id="App">
+      {/* 
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={greet}>
+          Greet
+        </button> */}
+      <div className="snow">●</div>
+      <div className="snow snow2nd">●</div>
+      <div className="container">
+        <div className="star">
+          <div className="treeCanvas">
+            <div className="tree top"></div>
+            <div className="tree middle"></div>
+            <div className="tree bottom"></div>
+            <div className="miki"></div>
+          </div>
+          <div className="moon">海洋無酸素事変</div>
+          <div className="image-container">
+            <img src={logo} id="logo1" alt="logo" width="30px" />
+            <img src={logo} id="logo2" alt="logo" width="30px" />
+            <img src={logo} id="logo3" alt="logo" width="30px" />
+            <img src={logo} id="logo1" alt="logo" width="30px" />
+            <img src={logo} id="logo2" alt="logo" width="30px" />
+            <img src={logo} id="logo3" alt="logo" width="30px" />
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default Curater;
